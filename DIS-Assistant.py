5973bac8fa9cd29556a37ec6e69d883383835da5
EclipseVision <eclipsevision@me.com>
2023-01-30 10:51:06 -0600

    Upddate code style

--- DIS-Assistant.py
+++ DIS-Assistant.py
@@ -2,22 +2,41 @@ import random
 
 version = 0.2
 
+
+def d2():
+    d2 = random.choice([1, 2])
+    return d2
+
+
+def d3():
+    d3 = random.choice([1, 2, 3])
+    return d3
+
+
 def d4():
     d4 = random.choice([1, 2, 3, 4])
     return d4
+
+
 def d6():
     d6 = random.choice([1, 2, 3, 4, 5, 6])
     return d6
 
+
+def d20():
+    d20 = random.choice(
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    )
+    return d20
+
+
 def ability():
-    BOY = d4() - d4()
-    DEX = d4() - d4()
-    SVY = d4() - d4()
-    TEC = d4() - d4()
-    print("BODY: " + str(BOY))
-    print("DEXTERITY: " + str(DEX))
-    print("SAVVY: " + str(SVY))
-    print("TECH: " + str(TEC))
+    abilities = {"BODY": d4() - d4()}
+    abilities.update({"DEXTERITY": d4() - d4()})
+    abilities.update({"SAVVY": d4() - d4()})
+    abilities.update({"TECH": d4() - d4()})
+    return abilities
+
 
 def origin():
     origin = {
@@ -26,14 +45,12 @@ def origin():
         3: "PUNK",
         4: "SOLPOD",
         5: "VELOCITY CURSED",
-        6: "VOID"
+        6: "VOID",
     }
     print(origin[d6()])
+
 
 if __name__ == "__main__":
     print("DEAD IN SPACE Game Assistant")
     print("v" + str(version))
     print("Ability Values")
-    ability()
-    print("Origin")
-    origin()
